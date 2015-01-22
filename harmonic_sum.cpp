4921d8a7dfefe30551684ae4a926040ad8d44ee7
christarazi <christarazi@users.noreply.github.com>
2015-01-22 11:39:13 -0800

    Added Kahan sum option
    For more precise calculations

--- harmonic_sum.cpp
+++ harmonic_sum.cpp
@@ -1,65 +1,133 @@
 #include <iostream>
 #include <iomanip>
 #include <thread>
+#include <future>
 #include <chrono>
+#include <vector>
+#include <cstddef>
+#include <cmath>
 
-const int NUM_THREADS = 4;
+/* H(N) is defined to be the harmonic partial sum to N terms. */
 
-// H(N) is defined to be the harmonic partial sum to N terms.
-// calculateHarmonic(): function given to threads to compute H(N).
-// unsigned long start: starting point for the loop; different for each thread.
-// unsigned long N: amount of terms to compute; different for each thread; divided evenly by # of threads.
-// double *sum: pointer to a double which holds a thread's individual sum for H(N).
-void calculateHarmonic(long long start, long long N, double *sum)
+// Set number of threads to hardware capability. 
+const std::size_t numThreads = std::thread::hardware_concurrency();
+
+/*
+	calculateHarmonic(): function given to threads to compute H(N); returns the individual sum the thread was given to compute.
+	unsigned long start: starting point for the loop; different for each thread.
+	unsigned long N: amount of terms to compute; different for each thread; divided evenly by # of threads.
+*/
+double calculateHarmonic(long long start, long long N)
 {
+	double sum = 0.0;
 	for(; start <= N; start++)
 	{
-		*sum += 1.0/start;
+		sum += 1.0 / start;
 	}
+	return sum;
+}
+
+/* 
+	calculateHarmonicKahan(): function given to threads to compute H(N); returns the individual sum the thread was given to compute.
+		computes the sum in much more precise manner. Algorithm can be found here: https://en.wikipedia.org/wiki/Kahan_summation_algorithm
+	Parameters used are the same as the above function.
+*/
+double calculateHarmonicKahan(long long start, long long N)
+{
+	double sum = 0.0;
+	double c = 0.0;
+	for(; start <= N; start++)
+	{
+		double y = (1.0/start) - c;
+		double t = sum + y;
+		c = (t - sum) - y;
+		sum = t;
+	}
+	return sum;
 }
 
 void executeWithThreads(long long N)
 {
-	std::thread t[NUM_THREADS];			// using four threads
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	double sum[NUM_THREADS] = {0.0};	// array of doubles for each sum calculated by each thread
-	double finalSum = 0.0;	
+	std::chrono::time_point<std::chrono::system_clock> start, end;	// variables to count runtime
+	std::vector<std::future<double>> futures(numThreads);			// vector storing return value from each thread
+	double finalSum = 0.0;
+	bool sumMethod;		// indicates whether to use regular sum or Kahan sum
+	std::cout << "Enter 0 for Regular sum (faster but less precise)\n \tor \nEnter 1 for Kahan sum (slower but more precise): ";
+	std::cin >> sumMethod;
+	std::cout << "\n";
+
 	start = std::chrono::system_clock::now();
-
-	// Assign threads their task.
-	for(int i = 0; i < NUM_THREADS; i++)
+	if(sumMethod)
 	{
-		// thread([name of function], [starting point], [N], [address of individual element in sum array])
-		t[i] = std::thread(calculateHarmonic, (((N/NUM_THREADS)*i)+1), ((N/NUM_THREADS)*(i+1)), &(sum[i]));
+		// Assign threads their task.
+		for(auto i = 0; i < numThreads; i++)
+		{
+			// std::async([launch policy], [name of function], [starting point], [N for each thread])
+			futures[i] = std::async(std::launch::async, calculateHarmonicKahan, ((N / numThreads) * i) + 1, ((N / numThreads) * (i + 1)));
+		}
+	}
+	else
+	{
+		// Assign threads their task.
+		for(auto i = 0; i < numThreads; i++)
+		{
+			// std::async([launch policy], [name of function], [starting point], [N for each thread])
+			futures[i] = std::async(std::launch::async, calculateHarmonic, ((N / numThreads) * i) + 1, ((N / numThreads) * (i + 1)));
+		}
 	}
 
-	// Join threads to terminate and sum up all individual sums into finalSum.
-	for(int i = 0; i < NUM_THREADS; i++)
+	// Get future value from vector and sum it all up.
+	for(auto&& i : futures)
 	{
-		t[i].join();
-		finalSum += sum[i];
+		finalSum += i.get();
 	}
-
 	end = std::chrono::system_clock::now();
-	std::cout << "Harmonic sum of " << N  << " is " << std::setprecision(std::numeric_limits<double>::digits10 + 1) << finalSum << "\n";
-	std::cout << "Calculation took " << std::setprecision(std::numeric_limits<double>::digits10 + 1) 
-			  << std::chrono::duration<double> (end-start).count() << " seconds.\n";
+
+	if(sumMethod)
+	{
+		std::cout << "Harmonic Kahan sum of " << N  << " is " 
+				  << std::setprecision(std::numeric_limits<double>::digits10 + 1) << finalSum << "\n";
+	}
+	else
+	{
+		std::cout << "Harmonic sum of " << N  << " is " 
+				  << std::setprecision(std::numeric_limits<double>::digits10 + 1) << finalSum << "\n";
+	}
+	std::cout << "Calculation took " << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1) 
+			  << std::chrono::duration<double> (end-start).count() << " seconds with " << numThreads << " threads.\n";
 }
 
 void executeNoThreads(long long N)
 {
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 	double sum = 0.0;
+	bool sumMethod;		// indicates whether to use regular sum or Kahan sum
+	std::cout << "Enter 0 for Regular sum (faster but less precise)\n \tor \nEnter 1 for Kahan sum (slower but more precise): ";
+	std::cin >> sumMethod;
+	std::cout << "\n";
+
 	start = std::chrono::system_clock::now();
-
-	for(int i = 1; i <= N; i++)
+	if(sumMethod)
 	{
-		sum += 1.0/i;
+		sum = calculateHarmonicKahan(1, N);
 	}
-
+	else
+	{
+		sum = calculateHarmonic(1, N);
+	}
 	end = std::chrono::system_clock::now();
-	std::cout << "Harmonic sum of " << N << " is " << std::setprecision(std::numeric_limits<double>::digits10 + 1) << sum << "\n";
-	std::cout << "Calculation took " << std::setprecision(std::numeric_limits<double>::digits10 + 1)
+
+	if(sumMethod)
+	{
+		std::cout << "Harmonic Kahan sum of " << N << " is " 
+				  << std::setprecision(std::numeric_limits<double>::digits10 + 1) << sum << "\n";
+	}
+	else
+	{
+		std::cout << "Harmonic sum of " << N << " is " 
+				  << std::setprecision(std::numeric_limits<double>::digits10 + 1) << sum << "\n";
+	}
+	std::cout << "Calculation took " << std::fixed << std::setprecision(std::numeric_limits<double>::digits10 + 1)
 			  << std::chrono::duration<double> (end-start).count() << " seconds.\n";
 }
 
@@ -67,8 +135,10 @@ int main()
 {
 	long long N;
 
+	std::cout << numThreads << " threads usable.\n";
 	std::cout << "Enter the amount of terms to calculate the Harmonic sum to: ";
 	std::cin >> N;
+	
 	if(N <= 0)
 	{
 		// Using std::endl here to force a buffer flush.
@@ -76,21 +146,22 @@ int main()
 		std::cerr << "Returning " << EXIT_FAILURE << " from main()..." << std::endl;
 		return EXIT_FAILURE;
 	}
+
 	std::cout << "\n";
 
-	if(N > NUM_THREADS && N % NUM_THREADS == 0)
+	if(N > numThreads && N % numThreads == 0)
 	{
-		std::cout << "Calculating with threads...\n";
+		std::cout << "Calculating with threads...\n\n";
 		executeWithThreads(N);
 	}
-	else if(N > NUM_THREADS && N % NUM_THREADS != 0)
+	else if(N > numThreads && N % numThreads != 0)
 	{
-		std::cout << "Calculating without threads...\n";
+		std::cout << "Calculating without threads...\n\n";
 		executeNoThreads(N);
 	}
 	else
 	{
-		std::cout << "Calculating without threads due to small N...\n";
+		std::cout << "Calculating without threads due to small N...\n\n";
 		executeNoThreads(N);
 	}
 
